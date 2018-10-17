868c2f587a1d752f8e75aeb9bb3720b097c37395
Richard Yen <richyen3@gmail.com>
2018-10-17 16:32:06 -0700

    Updates for 2.x * Adding support for incremental backup * Move init into Dockerfile * Provide example for incremental backup

--- bart/2.1/bart_test.sh
+++ bart/2.1/bart_test.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-psql -c "CREATE USER repuser REPLICATION"
+### Please run this script as root
+BART_PATH=/usr/edb/bart/bin
 
-mkdir /tmp/bart_backups
-
-bart init
+service edb-as-10 restart
 
 for ((i=0;i<1000;i++));
 do
   psql -c "CREATE TABLE table${i} (id serial primary key, first_name text not null default md5(random()::text), last_name text not null default md5(random()::text))" edb
 	psql -c "INSERT INTO table${i} VALUES (generate_series(1,1000),default,default)" edb
-	done
+done
 
-bart backup -s epas9x
+sudo -u enterprisedb ${BART_PATH}/bart backup -s epas --backup-name full1
+
+for ((i=0;i<1000;i++));
+do
+  psql -c "CREATE TABLE more_table${i} (id serial primary key, first_name text not null default md5(random()::text), last_name text not null default md5(random()::text))" edb
+	psql -c "INSERT INTO more_table${i} VALUES (generate_series(1,1000),default,default)" edb
+done
+
+sudo -iu enterprisedb ${BART_PATH}/bart backup -s epas --backup-name inc1 --parent full1 -F p
+
+sudo -u enterprisedb ${BART_PATH}/bart show-backups -s epas
