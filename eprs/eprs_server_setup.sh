d34b90a231ea3d2f244efd95eb82731eee7a143c
Richard Yen <richyen3@gmail.com>
2019-05-02 00:51:27 -0700

    reordering commands as per quickstart guide

--- eprs/eprs_server_setup.sh
+++ eprs/eprs_server_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 export PGVERSION=10
+export hostip=10.111.221.11
+export eprs_client="/usr/edb/rs-7.0/client/bin/runRepCLI.sh"
+
 sed -i -e "s/wal_level=hot_standby/wal_level=logical/" /var/lib/edb/as${PGVERSION}/data/postgresql.conf 
 
 cat <<EOT >> /var/lib/edb/as${PGVERSION}/data/postgresql.conf 
@@ -15,13 +18,14 @@ then
   #/usr/edb/rs-7.0/server/bin/runServer.sh --host 10.111.221.11 --config /usr/edb/rs-7.0/server/etc &
 else
   systemctl start edb-as-${PGVERSION}
-  #systemctl start edb-rs-server
+  sed -i "s/ngen.server.host=localhost/ngen.server.host=${hostip}/" /usr/edb/rs-7.0/server/etc/application.properties
+  systemctl start edb-rs-server
 fi
 psql -c "alter user enterprisedb identified by adminedb"
 pgbench -i
+${eprs_client} -joinnetwork -servername leaderNode -host ${hostip} -port 8082
 
 
-#eprs_client -setadminpassword -savepassword
+${eprs_client} -setadminpassword -savepassword
 echo 'adminedb' >/tmp/dbpassword.txt
-#/usr/edb/rs-7.0/client/bin/runRepCLI.sh -encrypt -input /tmp/dbpassword.txt -output /tmp/dbpassword.out
-
+${eprs_client} -encrypt -input /tmp/dbpassword.txt -output /tmp/dbpassword.out -user admin
--- eprs/eprs_setup.sh
+++ eprs/eprs_setup.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-eprs_client -joinnetwork -servername leaderNode -host 10.111.221.13 -port 8082 -user admin
-eprs_client -adddb \
-            -servername leaderNode \
-            -dbid node1db \
+node_num=3
+host_ip="10.111.221.1${node_num}"
+export eprs_client="/usr/edb/rs-7.0/client/bin/runRepCLI.sh"
+${eprs_client} -joinnetwork -servername Node${node_num} -host ${host_ip} -port 8082 -user admin
+${eprs_client} -adddb \
+            -servername Node${node_num} \
+            -dbid node${node_num}db \
             -dbtype enterprisedb \
-            -dbhost 10.111.221.13 \
+            -dbhost ${host_ip} \
             -dbport 5444 \
             -dbuser enterprisedb \
             -dbpassword N7Ryv4TGFInSPnvctbilyg== \
             -database edb \
             -user admin
 
-eprs_client -createpub \
+${eprs_client} -createpub \
             -pubname samplepub \
-            -servername leaderNode \
-            -dbid node1db \
+            -servername Node${node_num} \
+            -dbid node${node_num}db \
             -nodetype RW \
             -tables public.rep_test \
             -user admin
 
-eprs_client -joinpub -servername remoteNode2 -dbid node2db -nodetype RW -pubname samplepub -user admin
+${eprs_client} -joinpub -servername remoteNode${node_num} -dbid node${node_num}db -nodetype RW -pubname samplepub -user admin
 
-eprs_client -startsnapshot -pubname samplepub -dbid node2db -user admin
+${eprs_client} -startsnapshot -pubname samplepub -dbid node${node_num}db -user admin
 
-eprs_client -startstreaming -pubname samplepub -dbid node2db -user admin
+${eprs_client} -startstreaming -pubname samplepub -dbid node${node_num}db -user admin
